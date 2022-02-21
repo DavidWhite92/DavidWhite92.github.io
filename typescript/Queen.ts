c40555a1b97ca00203caf2378a18a51a9e9f58b9
DavidWhite92 <88293281+DavidWhite92@users.noreply.github.com>
2022-02-21 01:02:12 +0100

    Update Queen.ts

--- typescript/Queen.ts
+++ typescript/Queen.ts
@@ -497,6 +497,7 @@ let italia = [ava, divinity, elecktra, enorma, farida, ivana, riche, luquisha];
 //SPECIAL
 
 let pangina = new Queen("Pangina Heals", 10, 10, 9, 12, 9, 13, 11);
+let missfortune = new Queen("Miss Fortune", 15, 14, 15, 14, 15, 15, 15);
 
 let international_as = [baga, blu, cheryl, janey, jimbo, jujubee, lemon, monique, pangina];
 
@@ -541,7 +542,7 @@ anita, art, cocoj, elektra, etc, jojo, karen, kita, maxi,
 arantxa, carmenf, dovima, drag, hugaceo, inti, killer, pupi, sagittaria, macarena,
 adriana, beth, eve, giametric, icesis, kendall, kimoraA, oceane, pythia, stephanie, suki, synthia,
 ava, divinity, elecktra, enorma, farida, ivana, riche, luquisha,
-pangina
+pangina, missfortune,
 ].concat(allCustomQueens).sort((a, b) => a.getName().toLowerCase().localeCompare(b.getName().toLowerCase()));
 
 let allQueensCopy: Array<Queen> = [];
